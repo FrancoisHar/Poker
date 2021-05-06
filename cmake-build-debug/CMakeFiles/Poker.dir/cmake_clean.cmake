eb519f1676d29d831107b528f462fb736c396097
mentcley <72742704+mentcley@users.noreply.github.com>
2021-05-06 14:14:41 +0200

    poker on my branch

---
+++ cmake-build-debug/CMakeFiles/Poker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Poker.dir/main.cpp.obj"
+  "CMakeFiles/Poker.dir/src/Card.cpp.obj"
+  "Poker.exe"
+  "Poker.exe.manifest"
+  "Poker.lib"
+  "Poker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Poker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
