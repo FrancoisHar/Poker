7bac32365fbc70806363be1627e0000312b0145d
FrancoisHar <francoisharcourt@student.yncrea.fr>
2021-05-21 15:25:24 +0200

    ComparaisonMain/modifV2

--- src/ComparaisonMain.cpp
+++ src/ComparaisonMain.cpp
@@ -6,32 +6,57 @@
 #include <vector>
 using namespace std;
 
+//test
+int pair = 9; // la valeur de la carte renvoyé
+int doublePair = 6; // la carte la plus haute dans la double pair
+int brelan = 7;// la plus haute du brelan c'est le 7
+int suite = 3; // la carte la plus petite ded le suite (en compter 5)
+int flush = 10; //plus haute carte
+int full_house = 8;//la  carte la plus haute
+int carre = 12; //la carte la plus haute
+int quinteFlush = 12; //la carte la plus haute;
+
+
 int joueur1 = 4;
-int joueur2 = 7;
+int joueur2 = 3;
+
+std::string joueur;
 
 void NomsDesMain(int Score)
 {
 
     // serie de if pour afficher les messsages
-    if ( Score == 2 )
+    if ( Score == 1)
     {
-        std::cout << "... a gagne par une pair" << endl;
+        std::cout << " " << joueur << "a gagne par une pair" << endl;
+    }
+    else if (Score == 2)
+    {
+        std::cout << " " << joueur << "a gagne par une double pair" << endl;
     }
     else if (Score == 3)
     {
-        std::cout << "... a gagne par un triple" << endl;
+        std::cout << " " << joueur << "a gagne par un triple" << endl;
     }
     else if (Score == 4)
     {
-        std::cout << "... a gagne par une double pair " << endl;
+        std::cout << " " << joueur <<" a gagne par une suite " << endl;
     }
     else if (Score == 5)
     {
-        std::cout << "... a gagne par une full house " << endl;
+        std::cout <<  " " << joueur << "a gagne par une flush " << endl;
     }
     else if (Score == 6)
     {
-        std::cout << "... a gagne par un carre " << endl;
+        std::cout << " " << joueur  <<" a gagne par un full " << endl;
+    }
+    else if (Score == 7)
+    {
+        std::cout << " " << joueur << " a gagne par un carre " << endl;
+    }
+    else if (Score == 8)
+    {
+        std::cout << " " << joueur << " a gagne par une quinte flush " << endl;
     }
     else { cout << "Erreur la combinaison n'est pas dans la liste" << endl; }
 
@@ -44,13 +69,17 @@ int main()
 
     if (joueur1 > joueur2)
     {
-        std::cout << "joueur1 gagne" << endl; // je debug
+        //std::cout << "joueur1 gagne" << endl; // je debug
+
+        joueur = "joueur1";
         NomsDesMain(joueur1);
+        //std::cout << joueur << endl;   //debug
     }
     else if (joueur1 < joueur2)
     {
+        //std::cout << "joueur2 gagne" << endl; // je debug
 
-        std::cout << "joueur2 gagne" << endl; // je debug
+        joueur = "joueur2";
         NomsDesMain(joueur2);
     }
     else if (joueur1=joueur1)
